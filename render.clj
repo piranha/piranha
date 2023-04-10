83d197abbaf354ed957bb9c301f8d12b0eb4e93f
Alexander Solovyov <alexander@solovyov.net>
2023-04-10 23:39:40 +0300

    now a proper template
    why? idk felt like this 😁

--- render.clj
+++ render.clj
@@ -2,35 +2,37 @@
 
 (require
   '[clojure.data.xml :as xml]
-  '[clojure.string :as str])
+  '[selmer.parser :as t])
 
 
 (def FEED "https://solovyov.net/blog.atom")
-(def HEAD
-  "Github is just code with no context! Find out more:
-
-- follow me on Twitter [@asolovyov](https://twitter.com/asolovyov)
-- subscribe to my Telegram channel [@bitethebyte](https://t.me/bitethebyte) (it's in Ukrainian)
-- read my [blog](https://solovyov.net/)
-
-Here are latest posts from the blog:")
 
 
 (defn tag= [tag nodes]
   (filter #(= (:tag %) tag) nodes))
 
 
-(def data (-> (slurp FEED)
-              (xml/parse-str :namespace-aware false)))
+(def blog-data (-> (slurp FEED)
+                   (xml/parse-str :namespace-aware false)))
 
 
 (def blog-posts
-  (for [entry (tag= :entry (:content data))
+  (for [entry (tag= :entry (:content blog-data))
         :let  [title (first (tag= :title (:content entry)))
                link  (first (tag= :link (:content entry)))]]
-    (format "- [%s](%s)"
-      (apply str (:content title))
-      (:href (:attrs link)))))
+    {:title (apply str (:content title))
+     :url   (:href (:attrs link))}))
 
 
-(println (str/join "\n" (cons HEAD blog-posts)))
+(println
+  (t/render "Github is just code with no context! Find out more:
+
+- follow me on Twitter [@asolovyov](https://twitter.com/asolovyov)
+- subscribe to my Telegram channel [@bitethebyte](https://t.me/bitethebyte) (it's in Ukrainian)
+- read my [blog](https://solovyov.net/)
+
+Here are latest posts from the blog:
+
+{% for post in posts %}- [{{ post.title }}]({{ post.url }})
+{% endfor %}"
+    {:posts blog-posts}))
